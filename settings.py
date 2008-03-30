8e9b74ccc635ea3ba719ee55a6a5425d89755de7
alon.levy <alon@saymoo.org>
2008-03-30 21:15:15 +0300

    changes to run locally (and not on www.citytree.net)

--- settings.py
+++ settings.py
@@ -90,3 +90,26 @@ INSTALLED_APPS = (
 HEADER_MASK = '/home/tamizori/django/django_projects/citytree/frontpage/mask.png' #mask for main page header logo
 SITE_LOGO   = '' #site logo
 
+#--------------------------- Overrides ---------------------
+# Overriding stuff for local testing - remove when copying
+# back into citytree.net
+
+import os
+try:
+    if open('/etc/hostname').read().strip() == 'amber.saymoo.org':
+	DATABASE_HOST = ''
+	DATABASE_USER = 'root'
+	DATABASE_PASSWORD = 'sarduakar'
+	BASE_DIR = '/home/alon/src/citytree/citytree'
+	MEDIA_ROOT = BASE_DIR + '/siteMedia'
+	MEDIA_URL = 'http://localhost:8001/siteMedia'
+	ADMIN_MEDIA_PREFIX = 'http://localhost:8001/admin_media/'
+        TEMPLATE_DIRS = (
+            BASE_DIR + '/templates'
+        )
+        HEADER_MASK = BASE_DIR + '/frontpage/mask.png' #mask for main page header logo
+        DEBUG = True 
+        TEMPLATE_DEBUG = DEBUG
+except:
+    pass
+
