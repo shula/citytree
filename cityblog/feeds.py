363add52048862862366fc2ca9e86d851f1de969
alon.levy <alon@saymoo.org>
2008-04-21 18:50:18 +0300

    some more rss work (even if not on it's branch - maybe branches are best left for work that takes more then 5 minutes). now there are blog specific posts. still no connection from site.

--- cityblog/feeds.py
+++ cityblog/feeds.py
@@ -1,9 +1,21 @@
 from django.contrib.syndication.feeds import Feed
-from cityblog.models import post
+from cityblog.models import post, blog
 
 class LatestPosts(Feed):
     title = "Citytree Latest Posts"
-    link = "/latestposts/"
+    link = "/posts/"
     description = "Post updates for www.citytree.net"
-    def items(self):
-        return post.objects.filter(draft=0).order_by('-post_date')[:10]
+
+    def get_object(self, bits):
+        """ this is used to return a slug specific feed when bits is length one, like
+        """
+        if len(bits) == 1:
+            return blog.objects.get(slug=bits[0])
+        return None
+
+    def items(self, blog=None):
+        if blog is None:
+            return post.objects.filter(draft=0).order_by('-post_date')[:10]
+        # return only posts from the same blog
+        return post.objects.filter(draft=0, blog=blog).order_by('-post_date')[:10]
+
