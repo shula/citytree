659225fcc2f4baed2a239c254c99121381e606c9
City Tree <citytree@saymoo.org>
2008-06-21 12:20:58 +0300

    litrom infrastructure. login div is absolute, not relative

--- utils/litrom.py
+++ utils/litrom.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python2.4
+# -*- coding: utf-8 -*-
+
+
+import re
 
 from twill.commands import *
 from twill import get_browser
 
 from litrom_settings import LITROM_PASSWORD, LITROM_USERNAME
 
-def get_csv():
+def get_csv_html():
     b=get_browser()
 
     go('https://www.litrom.com/mng/')
@@ -19,7 +23,19 @@ def get_csv():
 
     return b.get_html()
 
+re_fields=re.compile('(?P<field>[^,]+)\s*,\s*')
+
+def get_donors(txt):
+    lines = [l for l in [l.strip() for l in txt[txt.index('>', txt.index('body'))+1:txt.index('_____')].split('\r')] if l not in ['', '<br>']]
+    lines_u=[[unicode(s, 'windows-1255') for s in re.findall(re_fields, line)] for line in lines]
+    #טופל  קמפיין  סכום  תאריך  שם פרטי  שם משפחה  כתובת  עיר  מיקוד  דואר אלקטרוני
+    fields = ['handled', 'campaign', 'amount', 'date', 'first', 'last', 'address', 'city', 'mikud', 'email']
+    donors = [dict(zip(fields, line)) for line in lines_u[1:]]
+    return donors
+	
 if __name__ == '__main__':
-    output = get_csv()
+    output = get_csv_html()
     open('output.csv','w+').write(output)
     print output
+    print get_donors(output)
+
