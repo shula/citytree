7995a1cd0d99a5f2324d84ab096b9c4de3b3ce6c
Alon Levy <alonlevy1@gmail.com>
2008-11-01 15:45:35 +0000

    more mistaken changes

--- workshop/templatetags/hebrewtime.py
+++ workshop/templatetags/hebrewtime.py
@@ -17,5 +17,9 @@ def hebrew_timeuntil(time1, time2=None):
         return u'%s שעות' % hr
     return u'%s שעות וחצי' % hr
 
-register.filter('hebrew_timeuntil', hebrew_timeuntil)
+def hebrew_weekday(dt):
+    # monday is 0, .., sunday is 6 - yuck
+    return ['שני','שלישי','רביעי','חמישי','שישי','שבת','ראשון'][dt.weekday()]
 
+register.filter('hebrew_timeuntil', hebrew_timeuntil)
+register.filter('hebrew_weekday', hebrew_weekday)
