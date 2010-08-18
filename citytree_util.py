916b90d5ef3f33ee3bc0b7f9bb948dfbaa80afce
Alon Levy <alonlevy1@gmail.com>
2010-08-18 12:55:52 -0700

    adding questionable comment to questionable add_render_variable function

--- citytree_util.py
+++ citytree_util.py
@@ -2,6 +2,9 @@ from django.contrib.sites.models import Site
 from cityblog.models import Blog, Post, Flag, Subject
 
 def add_render_variables(d, obj=None):
+    """ this function is used to add variables to several views request context's,
+    right now just for like / addtoany function.
+    """
     # TODO: put some stuff (like_href) into context variables
     # the menu's blogs are based on the db blogs, taken from displayed_blogs.
     d['displayed_blogs'] = Blog.objects.filter(display_in_menu=True)
